4ba69a89a8cac05344802cffbdb22e0d24c54db7
VladyslavPodrazhanskyi <https://VladyslavPodrazhanskyi@github.com>
2020-06-07 13:02:32 +0300

    commit last changes before sqlalchemy topic

--- run.py
+++ run.py
@@ -1,8 +1,24 @@
 from pprint import pprint
 from flask import Flask, request, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# app.config.update(
+#     SECRET_KEY=b'\x84\xeb\xf3*o\x06\xfeX\xd6\x1b]$9\xa8)2\xdd\xaaLkxa\xc90',
+#     # SQLALCHEMY_DATABASE_URI='<database>://<user.id>:<password>@<server>/<db_name>',
+#     SQLALCHEMY_DATABASE_URI='postgresql://postgres:<???>@localhost/catalog_db,
+#     SQLALCHEMY_TRACK_MODIFICATIONS=False
+# )
+# Config of postgres DB cursor
+# DATABASE = {
+#     "database": "cursor_db",
+#     "user": "cursor",
+#     "password": "very_secret_password",
+#     "port": 5432
+
+#
+# db = SQLAlchemy(app)
 
 
 table = {'movie_1': 134.23,
@@ -30,7 +46,7 @@ def query_strings(greeting="Hello"):
     return f"<h1>The greeting is {query_val}</h1>"
 
 
-@app.route('/user')
+@app.route('/user/')
 @app.route('/user/<name>')
 def no_query_string(name='Mila'):
     return f'Hello, there!, {name}'
@@ -48,6 +64,9 @@ def filters():
     return render_template('filters.html', table=table,
                            name=name, film=film)
 
+@app.route('/macros')
+def jinja_macros():
+    return render_template('using_macros.html', movies=table)
 
 if __name__ == "__main__":
     app.run(debug=True)
