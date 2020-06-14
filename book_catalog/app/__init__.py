455f912cce2b3bec2a61c3658a41b10c0535f797
VladyslavPodrazhanskyi <https://VladyslavPodrazhanskyi@github.com>
2020-06-14 21:33:22 +0300

    finished 2 layout, home and publisher pages

--- book_catalog/app/__init__.py
+++ book_catalog/app/__init__.py
@@ -4,9 +4,13 @@ import os
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
 from config import dev
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
+
 
 def create_app(config_type): #dev, test or prod
     app = Flask(__name__)
@@ -15,6 +19,7 @@ def create_app(config_type): #dev, test or prod
     app.config.from_pyfile(configuration)
 
     db.init_app(app)
+    bootstrap.init_app(app)
 
     from app.catalog import main
     app.register_blueprint(main)
--- book_catalog/app/catalog/routes.py
+++ book_catalog/app/catalog/routes.py
@@ -6,6 +6,11 @@ from app.catalog.models import Book, Publication
 @main.route('/')
 def display_books():
     books = Book.query.all()
-    return render_template('books.html', books=books)
+    return render_template('home.html', books=books)
 
 
+@main.route('/display/publisher/<int:publisher_id>')
+def display_publisher(publisher_id):
+    publisher = Publication.query.get(publisher_id)
+    publisher_books = Book.query.filter_by(pub_id=publisher_id).all()
+    return render_template('publisher.html', publisher_books=publisher_books, publisher=publisher)
