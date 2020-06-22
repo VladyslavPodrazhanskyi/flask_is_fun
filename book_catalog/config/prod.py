66d936b43fc57bdd7b71b6126c9263cd8d7b0f85
VladyslavPodrazhanskyi <https://VladyslavPodrazhanskyi@github.com>
2020-06-22 21:17:40 +0300

    first commit

--- book_catalog/config/prod.py
+++ book_catalog/config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY = b'H\xa9U<>\xa8\xfaO\x07\xbd\xec\xe1bJnk\xd0\xf7j\xceW\x8a\xe7/'
-SQLALCHEMY_DATABASE_URI='postgresql://pvv:krpp@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- book_catalog/run.py
+++ book_catalog/run.py
@@ -2,16 +2,16 @@ from app import create_app, db
 from app.auth.models import User
 from flask import request
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    # Use  app_context() in a
-    # with block, and everything that runs in the block
-    # will have access to current_app.
-    with flask_app.app_context():
-        db.create_all()
-        if not User.query.filter_by(name='Yaroslav').first():
-            User.create_user('Yaroslav',
-                             'yaric@gmail.com',
-                             'top_secret')
+
+flask_app = create_app('prod')
+# Use  app_context() in a
+# with block, and everything that runs in the block
+# will have access to current_app.
+with flask_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(name='Yaroslav').first():
+        User.create_user('Yaroslav',
+                         'yaric@gmail.com',
+                         'top_secret')
 
     flask_app.run()
